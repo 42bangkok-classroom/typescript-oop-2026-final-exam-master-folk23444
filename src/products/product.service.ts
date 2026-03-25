3dc6d033bf1ab4bf4b3cc9ffc7bb1ce31aa94509
event <event@z2t18c4.42bangkok.com>
2026-03-25 11:46:30 +0700

    5

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 import { ProductController } from './product.controller';
+import { Product } from './product.interface';
 
 @Injectable()
 export class ProductService {
     
-    findAll(){
-    const filePath = path.join(process.cwd(), 'data', 'products.json');
-    const data = fs.readFileSync(filePath, 'utf-8');
-    const user = JSON.parse(data);
-    return {user}
-    }
+    findAll(): Product[] {
+        const filePath = path.join(process.cwd(), 'data', 'product.json');
+        const data = fs.readFileSync(filePath, 'utf-8');
+        return JSON.parse(data) as Product[];
+}
 }
