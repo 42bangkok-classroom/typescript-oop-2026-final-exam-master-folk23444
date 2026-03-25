ecc2206164b617759b670a8d32657b554f203253
event <event@z2t18c4.42bangkok.com>
2026-03-25 10:53:33 +0700

    2

---
+++ src/products/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import { ProductController } from './product.controller';
+
+@Injectable()
+export class ProductService {
+    findAll(){
+    const filePath = path.join(process.cwd(), 'data', 'products.json');
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data)
+    }
+}
